af00d610f53ca6144509b629204f77f811cd4433
mirzad <varupamirzad@gmail.com>
2020-09-11 23:29:28 +0200

    Updates

---
+++ precache-manifest.b1b58ecf1e6f282f49ee7065c70f5310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "404dfa3e68193ab0b8b7c268cfbbeb3a",
+    "url": "/real-estate-agent-website/index.html"
+  },
+  {
+    "revision": "3bc9dc89c73b4f15d3e0",
+    "url": "/real-estate-agent-website/static/css/main.63856a93.chunk.css"
+  },
+  {
+    "revision": "431331b2cabf2b6c5cfb",
+    "url": "/real-estate-agent-website/static/js/2.022293c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/real-estate-agent-website/static/js/2.022293c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc9dc89c73b4f15d3e0",
+    "url": "/real-estate-agent-website/static/js/main.2beb0658.chunk.js"
+  },
+  {
+    "revision": "531270a073b5333049a1",
+    "url": "/real-estate-agent-website/static/js/runtime-main.c1d9b0be.js"
+  }
+]);
